994feaf83de91d171d1e07a74cb102be09570c5a
Julián Alberto Rosero <julianiq@MacBook-Air-de-Julian.local>
2022-07-05 20:58:31 -0500

    creación urls en carpeta principal

--- auth_ms/urls.py
+++ auth_ms/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('rest-auth/', include('rest_auth.urls')),
     path('rest-auth/registration/', include('rest_auth.registration.urls')),
+    path('soporte/', include('soporte.urls')),
+    path('factura/', include('factura.urls')),
+    path('proveedores/', include('proveedores.urls'))
 ]
